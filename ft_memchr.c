9ba83a9859dbda4d767e46d26880daf3938a0f33
Kevin Bertrand <kebertra@k1r4p12.42mulhouse.fr>
2025-11-02 17:40:23 +0100

    Fix: bug memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kebertra <kebertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 11:22:11 by kebertra          #+#    #+#             */
-/*   Updated: 2025/11/01 20:17:10 by kebertra         ###   ########.fr       */
+/*   Updated: 2025/11/02 17:39:13 by kebertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	s2 = (unsigned char *)s;
 	while (i < n)
 	{
-		if (s2[i] == c)
+		if (s2[i] == (unsigned char)c)
 		{
 			return ((void *)(s2 + i));
 		}
