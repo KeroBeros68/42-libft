a9b5f8d94737239a832d02b0c05925f97bec8227
Kevin Bertrand <kebertra@k1r2p7.42mulhouse.fr>
2025-11-28 13:41:30 +0100

    Fix: libft_fd.h include libft

--- Fd/libft_fd.h
+++ Fd/libft_fd.h
@@ -6,14 +6,14 @@
 /*   By: kebertra <kebertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 18:12:06 by kebertra          #+#    #+#             */
-/*   Updated: 2025/11/27 18:13:19 by kebertra         ###   ########.fr       */
+/*   Updated: 2025/11/28 13:40:55 by kebertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_FD_H
 # define LIBFT_FD_H
 
-# include "libft.h"
+# include "../libft.h"
 
 ssize_t	ft_putchar_fd(char c, int fd);
 ssize_t	ft_putstr_fd(char *s, int fd);
